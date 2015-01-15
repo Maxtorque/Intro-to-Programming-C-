b017a38807c538d8518779f6e12a1d24ed622944
Maxtorque <maxtorque@mail.bg>
2015-01-15 15:42:35 +0200

    Date and Time Homework

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class DateAndTime
+    {
+        static void Main()
+        {
+            //
+            // Write the current date and time.
+            //
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+        }
+    }
+}
