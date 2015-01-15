be9512bb62b45b940118ca677a9ad1000e955885
Maxtorque <maxtorque@mail.bg>
2015-01-15 15:37:47 +0200

    Age after 10 years homework

---
+++ AgeAfterTenYears/AgeAfterTenYears/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeAfterTenYears
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime birthDay = new DateTime(1989, 07, 12);
+            DateTime todaysDate = DateTime.Now;
+            var MyAge = todaysDate.Year - birthDay.Year;
+            Console.WriteLine("My Age is: " + MyAge);
+            var myFutureAge = MyAge + 10;
+            Console.WriteLine("My Age in 10 years will be: " + myFutureAge);
+
+        }
+    }
+}
