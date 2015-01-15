60df91a1ef68568c6fafde3f3dafee5ed6b2b3d5
Maxtorque <maxtorque@mail.bg>
2015-01-15 15:45:20 +0200

    Homework

---
+++ PrintSequence/PrintSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 2; i < 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0},", i);
+                }
+                else
+                {
+                    Console.Write("{0},", -i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Square Root/Square Root/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Root
+{
+    class Square_Root
+    {
+        static void Main()
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+    }
+}
